8436905a34784f9d802926963d8d8b43f750623c
Adrienne <adriennepaquin@gmail.com>
2021-07-20 11:06:34 -0400

    commit tree 43a066bda06ac6cd76e69459a28b735affb91a46

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # GET /plants
+  get '/plants', to: 'plants#index'
+
+  # GET /plants/id
+  get '/plants/:id', to: 'plants#show'
+
+  # POST /plants
+  post '/plants', to: 'plants#create'
 end
